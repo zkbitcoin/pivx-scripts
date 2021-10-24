e04f8b05c46c7d28773cf2ed8959643b2008e7f4
evaluatein <evaluatein@gmail.com>
2021-10-24 11:14:08 -0400

    sample graph testbed

---
+++ stats/js/pos-earnings.js
@@ -0,0 +1,338 @@
+// earnings data
+const json = "[\n" +
+    "  {\n" +
+    "    \"address\": \"D6ixy2j2qyYMPAibobymDkiXMEYSyKX3Mb\",\n" +
+    "    \"counters\": {\n" +
+    "      \"block_counters\": [\n" +
+    "        22,\n" +
+    "        44,\n" +
+    "        44,\n" +
+    "        44,\n" +
+    "        44\n" +
+    "      ],\n" +
+    "      \"date_counters\": {\n" +
+    "        \"ymdH\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24 08\",\n" +
+    "            \"count\": 34\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ymd\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-23\",\n" +
+    "            \"count\": 34\n" +
+    "          },\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24\",\n" +
+    "            \"count\": 14\n" +
+    "          },\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-25\",\n" +
+    "            \"count\": 4\n" +
+    "          },\n" +
+    "          {\n" +
+    "            \"date\": \"2021-11-25\",\n" +
+    "            \"count\": 50\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ym\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10\",\n" +
+    "            \"count\": 4\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"y\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021\",\n" +
+    "            \"count\": 4\n" +
+    "          }\n" +
+    "        ]\n" +
+    "      }\n" +
+    "    }\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"address\": \"STx39nArrm6fRBuo1QGm76Aax9YURGCiYi\",\n" +
+    "    \"counters\": {\n" +
+    "      \"block_counters\": [\n" +
+    "        12,\n" +
+    "        26,\n" +
+    "        26,\n" +
+    "        26,\n" +
+    "        26\n" +
+    "      ],\n" +
+    "      \"date_counters\": {\n" +
+    "        \"ymdH\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24 08\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ymd\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ym\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"y\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ]\n" +
+    "      }\n" +
+    "    }\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"address\": \"DBa5cB3hMns5kwrdWVuCx9JjR5s5sVCY7U\",\n" +
+    "    \"counters\": {\n" +
+    "      \"block_counters\": [\n" +
+    "        14,\n" +
+    "        22,\n" +
+    "        22,\n" +
+    "        22,\n" +
+    "        22\n" +
+    "      ],\n" +
+    "      \"date_counters\": {\n" +
+    "        \"ymdH\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24 08\",\n" +
+    "            \"count\": 6\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ymd\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24\",\n" +
+    "            \"count\": 6\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ym\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10\",\n" +
+    "            \"count\": 6\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"y\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021\",\n" +
+    "            \"count\": 6\n" +
+    "          }\n" +
+    "        ]\n" +
+    "      }\n" +
+    "    }\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"address\": \"DDU6BCfxp2eGdQ5AuoyL4QQo6D4abms5qg\",\n" +
+    "    \"counters\": {\n" +
+    "      \"block_counters\": [\n" +
+    "        4,\n" +
+    "        12,\n" +
+    "        12,\n" +
+    "        12,\n" +
+    "        12\n" +
+    "      ]\n" +
+    "    }\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"address\": \"DQqkQD2s4CgG2GAjcRXiT5crpXneLGHHVT\",\n" +
+    "    \"counters\": {\n" +
+    "      \"block_counters\": [\n" +
+    "        8,\n" +
+    "        8,\n" +
+    "        8,\n" +
+    "        8,\n" +
+    "        8\n" +
+    "      ]\n" +
+    "    }\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"address\": \"D6B4Sw89gsKveLEdG9G5GC49APq6VJdMi6\",\n" +
+    "    \"counters\": {\n" +
+    "      \"block_counters\": [\n" +
+    "        2,\n" +
+    "        4,\n" +
+    "        4,\n" +
+    "        4,\n" +
+    "        4\n" +
+    "      ],\n" +
+    "      \"date_counters\": {\n" +
+    "        \"ymdH\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24 08\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ymd\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ym\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"y\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ]\n" +
+    "      }\n" +
+    "    }\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"address\": \"D8xM8qYTxCngeWeMTzC5b5aRCMkAmLJUfk\",\n" +
+    "    \"counters\": {\n" +
+    "      \"block_counters\": [\n" +
+    "        2,\n" +
+    "        4,\n" +
+    "        4,\n" +
+    "        4,\n" +
+    "        4\n" +
+    "      ],\n" +
+    "      \"date_counters\": {\n" +
+    "        \"ymdH\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24 08\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ymd\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ym\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"y\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ]\n" +
+    "      }\n" +
+    "    }\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"address\": \"DDjPsKCkfgPwrSkAgqcZ4YZR9NTPTz6xFB\",\n" +
+    "    \"counters\": {\n" +
+    "      \"block_counters\": [\n" +
+    "        4,\n" +
+    "        4,\n" +
+    "        4,\n" +
+    "        4,\n" +
+    "        4\n" +
+    "      ]\n" +
+    "    }\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"address\": \"D9KY88xQV7SGevxcViREuWCVYdAyh2D4h1\",\n" +
+    "    \"counters\": {\n" +
+    "      \"block_counters\": [\n" +
+    "        2,\n" +
+    "        2,\n" +
+    "        2,\n" +
+    "        2,\n" +
+    "        2\n" +
+    "      ]\n" +
+    "    }\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"address\": \"DSnE53PN6Z1p1b8vcvDkEWmByiq23Cco4M\",\n" +
+    "    \"counters\": {\n" +
+    "      \"block_counters\": [\n" +
+    "        2,\n" +
+    "        2,\n" +
+    "        2,\n" +
+    "        2,\n" +
+    "        2\n" +
+    "      ],\n" +
+    "      \"date_counters\": {\n" +
+    "        \"ymdH\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24 08\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ymd\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10-24\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"ym\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021-10\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ],\n" +
+    "        \"y\": [\n" +
+    "          {\n" +
+    "            \"date\": \"2021\",\n" +
+    "            \"count\": 2\n" +
+    "          }\n" +
+    "        ]\n" +
+    "      }\n" +
+    "    }\n" +
+    "  }\n" +
+    "]"
+const earnings_data = JSON.parse(json);
+
+var xValue = []
+var yValue = []
+
+function getAddressData(address) {
+    return data.filter(
+        function(data){return data.code == code}
+    );
+}
+
+var today = new Date();
+var yyyymm = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2)
+
+for(var i = 0; i < earnings_data[0].counters.date_counters.ymd.length; i++) {
+    var date = earnings_data[0].counters.date_counters.ymd[i].date;
+    if (yyyymm == date.slice(0, -3)) {
+        var count = earnings_data[0].counters.date_counters.ymd[i].count;
+        xValue.push(date)
+        yValue.push(count)
+    }
+}
+
+var trace = {
+    x: xValue,
+    y: yValue,
+    type: 'bar',
+    text: yValue.map(String),
+    textposition: 'auto',
+    hoverinfo: 'none',
+    opacity: 0.5,
+    marker: {
+        color: 'rgb(158,202,225)',
+        line: {
+            color: 'rgb(8,48,107)',
+            width: 1.5
+        }
+    }
+};
+
+var data = [trace];
+
+var layout = {
+    title: 'POS Earnings'
+};
+
+Plotly.newPlot('root', data, layout);
