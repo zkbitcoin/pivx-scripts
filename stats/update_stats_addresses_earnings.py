64017d13e50d4fe6d9fb1f99e341144a29e32440
evaluatein <evaluatein@gmail.com>
2021-10-27 16:08:07 -0400

    update

--- stats/update_stats_addresses_earnings.py
+++ stats/update_stats_addresses_earnings.py
@@ -38,21 +38,21 @@ parser.add_argument('-r', '--runfile-location-path',
 parser.add_argument('-posp', '--output-location-pos-path',
                     required=False,
                     type=str,
-                    default="../../../../stats_data/address-earnings/pos/json",
+                    default="../../../../stats_data/address-rewards/pos/json",
                     dest="posloc",
                     metavar="<output-location-pos-path>",
                     help="output-location-pos-path")
 parser.add_argument('-mnp', '--output-location-mn-path',
                     required=False,
                     type=str,
-                    default="../../../../stats_data/address-earnings/mn/json",
+                    default="../../../../stats_data/address-rewards/mn/json",
                     dest="mnloc",
                     metavar="<output-location-mn-path>",
                     help="output-location-mn-path")
 parser.add_argument('-d', '--db-location-path',
                     required=False,
                     type=str,
-                    default="../../../../stats_data/address-earnings/db",
+                    default="../../../../stats_data/address-rewards/db",
                     dest="dbloc",
                     metavar="<output-location-path>",
                     help="output-location-path")
@@ -65,8 +65,10 @@ PROCESS_DATE_COUNTERS = 1
 
 args = parser.parse_args()
 
-address_earnings_run_file = os.path.join(args.rloc, "address_earnings_run.json")
-address_earnings_file = [os.path.join(args.posloc, "address_earnings_pos.json"), os.path.join(args.mnloc, "address_earnings_mn.json")]
+address_rewards_run_file = os.path.join(args.rloc, "address_rewards_run.json")
+address_rewards_block_counters_file = [os.path.join(args.posloc, "address_rewards_block_counters_pos.json"), os.path.join(args.mnloc, "address_rewards_block_counters_mn.json")]
+address_rewards_date_counters_file = [os.path.join(args.posloc, "address_rewards_date_counters_pos.json"), os.path.join(args.mnloc, "address_rewards_date_counters_mn.json")]
+
 
 options = rocksdb.Options()
 options.create_if_missing = True
@@ -74,25 +76,25 @@ options.create_if_missing = True
 first_time = False
 
 try:
-    with open(address_earnings_run_file, 'r') as f:
-        address_earnings_run = json.load(f)
+    with open(address_rewards_run_file, 'r') as f:
+        address_rewards_run = json.load(f)
 except FileNotFoundError:
     db = rocksdb.DB(args.dbloc, options, column_families={}, read_only=False)
-    db.create_column_family(b'address_earnings_pos_cf', rocksdb.ColumnFamilyOptions())
-    db.create_column_family(b'address_earnings_mn_cf', rocksdb.ColumnFamilyOptions())
+    db.create_column_family(b'address_rewards_pos_cf', rocksdb.ColumnFamilyOptions())
+    db.create_column_family(b'address_rewards_mn_cf', rocksdb.ColumnFamilyOptions())
     del db
-    address_earnings_run = {"block":1}
+    address_rewards_run = {"block":1}
     first_time = True
 
 column_families = {
     b'default': rocksdb.ColumnFamilyOptions(),
-    b'address_earnings_pos_cf': rocksdb.ColumnFamilyOptions(),
-    b'address_earnings_mn_cf': rocksdb.ColumnFamilyOptions(),
+    b'address_rewards_pos_cf': rocksdb.ColumnFamilyOptions(),
+    b'address_rewards_mn_cf': rocksdb.ColumnFamilyOptions(),
 }
 
 db = rocksdb.DB(args.dbloc, options, column_families=column_families, read_only=False)
-address_earnings_pos_cf = db.get_column_family(b'address_earnings_pos_cf')
-address_earnings_mn_cf = db.get_column_family(b'address_earnings_mn_cf')
+address_rewards_pos_cf = db.get_column_family(b'address_rewards_pos_cf')
+address_rewards_mn_cf = db.get_column_family(b'address_rewards_mn_cf')
 
 # Initialize RPC connection and API client
 httpConnection = httplib.HTTPConnection(rpc_host, rpc_port, timeout=20)
@@ -100,15 +102,35 @@ conn = AuthServiceProxy(rpc_url, timeout=1000, connection=httpConnection)
 blockCount = conn.getblockcount()
 
 address_array = [{}, {}]
+block_counters = []
+block_counters = [{} for i in range(PROCESS_DATE_COUNTERS+1)]
+date_counters = []
+date_counters = [{} for i in range(PROCESS_DATE_COUNTERS+1)]
 
-def db_to_date_counters(addresses, columnFamily):
+
+def db_to_date_counters(counters, columnFamily):
     it = db.iteritems(columnFamily)
     it.seek_to_first()
     for i in it:
-        k = str(i[0], "utf-8")
+        k = str(i[0][1], "utf-8")
         v = i[1]
-        addresses[k] = {}
-        addresses[k].update(ast.literal_eval(v.decode("utf-8")))
+        address_array[counters][k] = {}
+        address_array[counters][k].update(ast.literal_eval(v.decode("utf-8")))
+        date_counters[counters][k] = {}
+        date_counters[counters][k]['ymdH'] = OrderedDict()
+        date_counters[counters][k]['ymd'] = OrderedDict()
+        date_counters[counters][k]['ym'] = OrderedDict()
+        date_counters[counters][k]['y'] = OrderedDict()
+
+        for elem in address_array[counters][k]['dc']['ymdH']:
+            date_counters[counters][k]['ymdH'].update({elem['d']: elem['c']})
+        for elem in address_array[counters][k]['dc']['ymd']:
+            date_counters[counters][k]['ymd'].update({elem['d']: elem['c']})
+        for elem in address_array[counters][k]['dc']['ym']:
+            date_counters[counters][k]['ym'].update({elem['d']: elem['c']})
+        for elem in address_array[counters][k]['dc']['y']:
+            date_counters[counters][k]['y'].update({elem['d']: elem['c']})
+
         #print("Address %s date_counters %s " % (k, v))
     #print(addresses)
 
@@ -117,7 +139,7 @@ def db_prune(addresses):
     it = db.iteritems()
     it.seek_to_first()
     for i in it:
-        k = str(i[0], "utf-8")
+        k = str(i[0][1], "utf-8")
         if k is not addresses:
             #print("Delete %s address from db " % (k))
             db.delete(i[0])
@@ -127,12 +149,12 @@ def db_prune(addresses):
 
 def update_counters(addresses, address, pattern, date, reward):
     found = False
-    for e in addresses[address]['date_counters'][pattern]:
-        if e['date'] == date:
+    for e in addresses[address]['dc'][pattern]:
+        if e['d'] == date:
             found = True
-            e['count'] = e['count'] + reward
+            e['c'] = e['c'] + reward
     if not found:
-        addresses[address]['date_counters'][pattern].append({'date': date, 'count': reward})
+        addresses[address]['dc'][pattern].append({'d': date, 'c': reward})
 
 def get_counters(process_type, block_start, block_stop, order):
     j = 0
@@ -153,7 +175,6 @@ def get_counters(process_type, block_start, block_stop, order):
                         continue
                     if stake:
                         if "addresses" in vout["scriptPubKey"]:
-                            reward = 0
                             if i == ADDRESSES_POS:
                                 reward = 2
                             else:
@@ -161,10 +182,9 @@ def get_counters(process_type, block_start, block_stop, order):
                             tx_addresses = vout["scriptPubKey"]["addresses"]
                             address = tx_addresses[0]
                             if process_type == PROCESS_BLOCK_COUNTERS:
-                                if address not in address_array[i]:
+                                if address not in block_counters[i]:
                                     address_array[i][address] = {}
-                                if 'block_counters' not in  address_array[i][address]:
-                                    address_array[i][address]['block_counters'] = [0,0,0,0,0]
+                                    block_counters[i][address] = [0,0,0,0,0]
                                 if j <= 50:
                                     k = 0
                                 elif j <= 250:
@@ -176,67 +196,91 @@ def get_counters(process_type, block_start, block_stop, order):
                                 else:
                                     k = 4
                                 for l in range(k, 5):
-                                    address_array[i][address]['block_counters'][l] = address_array[i][address]['block_counters'][l] + reward
+                                    block_counters[i][address][l] = block_counters[i][address][l] + reward
+                                address_array[i][address]['bc'] = block_counters[i][address]
                                 #if i == ADDRESSES_POS:
-                                #    print("Updating POS Address %s block counters %s " % (address, address_array[i][address]['block_counters']))
+                                #    print("Updating POS Address %s block counters %s " % (address, address_array[i][address]['bc']))
                                 #else:
-                                #    print("Updating Masternode Address %s block counters %s " % (address, address_array[i][address]['block_counters']))
+                                #    print("Updating Masternode Address %s block counters %s " % (address, address_array[i][address]['bc']))
                             else: # PROCESS_DATE_COUNTERS
-                                if address not in address_array[i]:
+                                if address not in block_counters[i]:
                                     continue
                                 date = datetime.fromtimestamp(int(tx_raw["time"]))
                                 ymdH = date.strftime("%Y-%m-%d %H")
                                 ymd = date.strftime("%Y-%m-%d")
                                 ym = date.strftime("%Y-%m")
                                 y = date.strftime("%Y")
-                                if 'date_counters' not in  address_array[i][address]:
-                                    address_array[i][address]['date_counters'] = {}
-                                    address_array[i][address]['date_counters']['ymdH'] = []
-                                    address_array[i][address]['date_counters']['ymd'] = []
-                                    address_array[i][address]['date_counters']['ym'] = []
-                                    address_array[i][address]['date_counters']['y'] = []
-                                update_counters(address_array[i], address, 'ymdH', ymdH, reward)
-                                update_counters(address_array[i], address, 'ymd', ymd, reward)
-                                update_counters(address_array[i], address, 'ym', ym, reward)
-                                update_counters(address_array[i], address, 'y', y, reward)
+                                if address not in date_counters[i]:
+                                    address_array[i][address]["dc"] = {}
+                                    date_counters[i][address] = {}
+                                if 'ymdH' not in date_counters[i][address]:
+                                    date_counters[i][address]['ymdH'] = OrderedDict()
+                                if 'ymd' not in date_counters[i][address]:
+                                    date_counters[i][address]['ymd'] = OrderedDict()
+                                if 'ym' not in date_counters[i][address]:
+                                    date_counters[i][address]['ym'] = OrderedDict()
+                                if 'y' not in date_counters[i][address]:
+                                    date_counters[i][address]['y'] = OrderedDict()
+                                if ymdH not in date_counters[i][address]['ymdH']:
+                                    date_counters[i][address]['ymdH'][ymdH] = 0
+                                if ymd not in date_counters[i][address]['ymd']:
+                                    date_counters[i][address]['ymd'][ymd] = 0
+                                if ym not in date_counters[i][address]['ym']:
+                                    date_counters[i][address]['ym'][ym] = 0
+                                if y not in date_counters[i][address]['y']:
+                                    date_counters[i][address]['y'][y] = 0
+
+                                date_counters[i][address]['ymdH'].update({ymdH: date_counters[i][address]['ymdH'][ymdH] + reward})
+                                date_counters[i][address]['ymd'].update({ymd: date_counters[i][address]['ymd'][ymd] + reward})
+                                date_counters[i][address]['ym'].update({ym: date_counters[i][address]['ym'][ym] + reward})
+                                date_counters[i][address]['y'].update({y: date_counters[i][address]['y'][y] + reward})
+
+                                address_array[i][address]["dc"]['ymdH'] = [{'d': key, 'c': value} for key, value in date_counters[i][address]['ymdH'].items()]
+                                address_array[i][address]["dc"]['ymd'] = [{'d': key, 'c': value} for key, value in date_counters[i][address]['ymd'].items()]
+                                address_array[i][address]["dc"]['ym'] = [{'d': key, 'c': value} for key, value in date_counters[i][address]['ym'].items()]
+                                address_array[i][address]["dc"]['y'] = [{'d': key, 'c': value} for key, value in date_counters[i][address]['y'].items()]
+
                                 if i == ADDRESSES_POS:
-                                    db.put((address_earnings_pos_cf, bytes(address, 'utf-8')), bytes(str(address_array[i][address]), 'utf-8'))
+                                    db.put((address_rewards_pos_cf, bytes(address, 'utf-8')), bytes(str(address_array[i][address]), 'utf-8'))
                                     #print("Updating POS Address %s date_counters %s " % (address, address_array[i][address]))
                                 else:
-                                    db.put((address_earnings_mn_cf, bytes(address, 'utf-8')), bytes(str(address_array[i][address]), 'utf-8'))
+                                    db.put((address_rewards_mn_cf, bytes(address, 'utf-8')), bytes(str(address_array[i][address]), 'utf-8'))
                                     #print("Updating Masternode Address %s date_counters %s " % (address, address_array[i][address]))
                             if (i := (i + 1)) > 1:
                                 break
 
-get_counters(PROCESS_BLOCK_COUNTERS, blockCount, blockCount-1000, -1)
+get_counters(PROCESS_BLOCK_COUNTERS, blockCount, blockCount-1000-1, -1)
 
 if first_time:
-    get_counters(PROCESS_DATE_COUNTERS, 2660000, blockCount, +1)
+    get_counters(PROCESS_DATE_COUNTERS, 2660000, blockCount+1, +1)
 else:
-    with open(address_earnings_run_file, 'r') as json_data:
+    with open(address_rewards_run_file, 'r') as json_data:
         d = json.load(json_data)
         json_data.close()
-        db_to_date_counters(address_array[ADDRESSES_POS], address_earnings_pos_cf)
-        db_to_date_counters(address_array[ADDRESSES_MN], address_earnings_mn_cf)
-        get_counters(PROCESS_DATE_COUNTERS, d["block"]+1, blockCount, +1)
+        db_to_date_counters(ADDRESSES_POS, address_rewards_pos_cf)
+        db_to_date_counters(ADDRESSES_MN, address_rewards_mn_cf)
+        get_counters(PROCESS_DATE_COUNTERS, d["block"]+1, blockCount+1, +1)
 
 try:
     for i in range (ADDRESSES_POS, ADDRESSES_MN+1):
-        address_earnings_array  = []
+        address_rewards_array  = []
+        for k, v in block_counters[i].items():
+            address_rewards_array.append({"a": k, "bc": v})
+        address_rewards_array = sorted(address_rewards_array, key = lambda i: i["bc"][4], reverse=True)
+        with open(address_rewards_block_counters_file[i], 'w+') as f:
+            json.dump(address_rewards_array , f)
 
+        address_rewards_array  = []
         for k, v in address_array[i].items():
-            address_earnings_array.append({"address": k, "counters": v})
-
-        address_earnings_array = sorted(address_earnings_array, key = lambda i: i["counters"]["block_counters"][4], reverse=True)
-
-        with open(address_earnings_file[i], 'w+') as f:
-            json.dump(address_earnings_array , f)
+            address_rewards_array.append({"a": k, "dc": v["dc"]})
+        with open(address_rewards_date_counters_file[i], 'w+') as f:
+            json.dump(address_rewards_array , f)
 
 except:
     raise
 finally:
-    with open(address_earnings_run_file, 'w+') as f:
-        address_earnings_run["block"] = blockCount
-        json.dump(address_earnings_run, f)
+    with open(address_rewards_run_file, 'w+') as f:
+        address_rewards_run["block"] = blockCount
+        json.dump(address_rewards_run, f)
 
 httpConnection.close()
